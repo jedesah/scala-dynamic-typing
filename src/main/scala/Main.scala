eab9e0ea36969bbe80a0d73c959d61454f22cb80
Jean-Rémi Desjardins <jeanremi.desjardins@gmail.com>
2015-04-16 09:49:00 -0700

    Further exploration

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,31 +1,68 @@
-package com.github.jedesah
+package com.github.jedesah.typelevel
 
+import scala.collection.mutable
 import scala.language.experimental.macros
+import scala.reflect.macros.TypecheckException
 import scala.reflect.macros.blackbox.Context
 
-package object failTypeCheckAtRuntime {
+package object test {
 
-  case class TypeCheckFailure(msg: String) extends Exception(msg)
+  def runtimeTypecheck(identUnderTest: Any)(code: String) = macro failTypeCheckAtRuntime
 
-  implicit class runtimeQuote(val ctx: StringContext) extends AnyVal {
-    def dyn(args: Any*): Any = macro failTypeCheckAtRuntime
+  def runtimeTypecheck(code: String) = macro failMacroTypeCheckAtRuntime
+  
+  def produceIncorrectCode: Unit = macro produceIncorrectCodeImpl
+  
+  def produceIncorrectCodeImpl(c: Context): c.Expr[Unit] = {
+    import c.universe._
+    c.Expr(q"4 ++ 5")
   }
 
-  def failTypeCheckAtRuntime(c: Context)(args: c.Expr[Any]*): c.Expr[Any] = {
+  def abortMacroExpansion: Unit = macro abortMacroExpansionImpl
+
+  def abortMacroExpansionImpl(c: Context): c.Expr[Unit] = {
+    c.abort(c.enclosingPosition, "Doomed to fail!")
+  }
+
+  def failMacroTypeCheckAtRuntime(c: Context)(code: c.Expr[String]): c.Expr[Any] = {
     import c.universe._
-    c.prefix.tree match {
-      case Apply(_, List(Apply(_, rawParts))) =>
-        if (rawParts.length != 1) c.abort(c.enclosingPosition, "Does not currently support actual String interpolation, funny heh?! :-)")
-        val code = rawParts.head.asInstanceOf[Literal].value.value.asInstanceOf[String]
-        val ast = c.parse(code)
-        val actualCode = util.Try(c.typecheck(ast)).recover{ case t =>
-          c.warning(c.enclosingPosition, t.getMessage)
-          val msg = Literal(Constant(t.getMessage))
-          q"""throw TypeCheckFailure($msg)"""
-        }
-        c.Expr(actualCode.get)
-      case _ => throw new Exception("Improper use of this macro")
+    val codeString = code.tree.asInstanceOf[Literal].value.value.asInstanceOf[String]
+    val ast = c.parse(codeString)
+    val typeCheckResult = util.Try(c.typecheck(ast, withMacrosDisabled = true))
+    if (typeCheckResult.isFailure) throw typeCheckResult.failed.get
+    val actualCode = util.Try(c.typecheck(ast)).recover{ case t: TypecheckException =>
+      new TypecheckException(NoPosition, t.msg)
+      val msg = Literal(Constant(t.getMessage))
+      c.warning(c.enclosingPosition, t.getMessage)
+      q"""throw TypecheckException(null,$msg)"""
     }
+    c.Expr(actualCode.get)
+  }
+
+  def failTypeCheckAtRuntime(c: Context)(identUnderTest: c.Expr[Any])(code: c.Expr[String]): c.Expr[Any] = {
+    import c.universe._
+    val codeString = code.tree.asInstanceOf[Literal].value.value.asInstanceOf[String]
+    val ast = c.parse(codeString)
+    object FindMacroLocations extends Traverser {
+      val locations: mutable.ListBuffer[Position] = mutable.ListBuffer()
+      override def traverse(tree: Tree): Unit = tree match {
+        case ident@Ident(name) => {
+          if (ident equalsStructure identUnderTest.tree) locations += ident.pos
+        }
+        case _ => super.traverse(tree)
+      }
+      def apply(tree: Tree) = {traverse(tree); locations.toList}
+    }
+    val locations = FindMacroLocations(ast)
+    val actualCode = util.Try(c.typecheck(ast)).recover{ case t: TypecheckException =>
+      val msg = Literal(Constant(t.getMessage))
+      if (locations.exists(_.start == t.pos.start)) {
+        c.warning(c.enclosingPosition, t.getMessage)
+        q"""throw TypeCheckException(null, $msg)"""
+      }
+      else c.abort(c.enclosingPosition, t.getMessage)
+    }
+    c.Expr(actualCode.get)
   }
 }
 
--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -1,28 +1,37 @@
-package com.github.jedesah.failTypeCheckAtRuntime
+package com.github.jedesah.typelevel.test
 
 import org.specs2.mutable._
 import shapeless.test.illTyped
 
+import scala.reflect.macros.TypecheckException
+
 class MainSpec extends Specification {
 
   """dyn"code"""" should {
-    "fail at runtime if the string does not typecheck as code" in {
-      dyn"5 ++ 6" should throwA[TypeCheckFailure]
+    "fail at compile time if a normal compile error" in {
+      illTyped { """runtimeTypecheck{ "5 ++ 6" }""" }
+      ok
     }
     "fail at compile time if it is completely invalid syntax'" in {
-      illTyped { """dyn"5..9&#"""" }
+      illTyped { """runtimeTypecheck{ "5..9&#" }""" }
       ok
     }
     "work fine otherwise" in {
-      "when dealing with references from Predeg" in {
-        val a = dyn"5 + 6"
+      "when dealing with values from Predef" in {
+        val a = runtimeTypecheck {"5 + 6"}
         a === 11
       }
-      "not require qualifying local references" in {
+      "not require fully qualifying values from local scope" in {
         case class Cat(name: String, age: Int)
         val myCat = Cat("Fluf", 10)
-        val age = dyn"myCat.age"
+        val age = runtimeTypecheck{"myCat.age"}
         age === 10
+      }
+      "produces incorrect code" in {
+        runtimeTypecheck{"produceIncorrectCode"} must throwA[TypecheckException]
+      }
+      "macro expansion aborts" in {
+        runtimeTypecheck{"abortMacroExpansion"} must throwA[TypecheckException]
       }
     }
   }
